d5f81c6235d1a55cfa238647efb77e81fdf9428f
Armanahmed12 <mdarmanahmed1232@gmail.com>
2024-03-28 10:46:49 +0600

    firebase tool has been implemented

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -10,9 +11,10 @@ const firebaseConfig = {
   projectId: "emajon-lite-website",
   storageBucket: "emajon-lite-website.appspot.com",
   messagingSenderId: "1039849878045",
-  appId: "1:1039849878045:web:d541aef70c09cdab54ccf4"
+  appId: "1:1039849878045:web:4921139d273905d554ccf4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
